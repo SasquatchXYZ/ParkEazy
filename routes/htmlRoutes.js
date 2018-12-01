b7c77be09631cb97083e0f8142e6c7d018e56c98
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2018-12-01 13:03:16 -0500

    added html routes for ease of front end design

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,5 +1,18 @@
-const db = require('../models');
+const db = require("../models");
 
 module.exports = function(app) {
+  app.get("/", function(req, res) {
 
+    res.render("index", { testMessage: "This is the landing page..." });
+  });
+
+  app.get("/search", function(req, res) {
+
+    res.render("search", { testMessage: "This is the search page..." });
+  });
+
+  app.get("/create", function(req, res) {
+
+    res.render("create", { testMessage: "This is the create page..." });
+  });
 };
